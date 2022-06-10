ac045f0294219888de3c540505e99881068c1c68
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-10 19:15:21 +0900

    feat: admin 추가

--- quest/admin.py
+++ quest/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
+from .models import UserProfile
+from .models import Hobby
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(UserProfile)
+admin.site.register(Hobby)
