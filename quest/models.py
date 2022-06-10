8ae0e3342304e943ea253b12f6337ec55249919f
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-10 19:01:16 +0900

    Feat: User,Hobby,UserProfile 추가

--- quest/models.py
+++ quest/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username=models.CharField(max_length=20,unique=True)
+    email=models.EmailField(max_length=20,unique=True)
+    password=models.CharField(max_length=60)
+    fullname=models.CharField(max_length=20)
+    join_date=models.DateTimeField(auto_now_add=True)
+
+class UserPofile(models.Model):  #User와 일대일관계/ Hobby와 다대다관계
+    user=models.OneToOneField(to=User, verbose_name="사용자", on_delete=models.CASCADE)
+    hobby=models.ManyToManyField(to="Hobby",verbose_name="취미")#Hobby클래스는 아래에 있으므로
+    introduction= models.TextField()
+    birthday=models.DateField()
+    age= models.IntegerField()
+
+class Hobby(models.Model):
+    name=models.CharField( max_length=50)
+
