779ca632781dfe460d460998e485bc242d03f7b9
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-13 19:14:14 +0900

    Refactor: admin형식설정

--- quest/admin.py
+++ quest/admin.py
@@ -4,10 +4,13 @@ from .models import User,UserProfile,Hobby
 
 
 # Register your models here.
+class HobbyAdmin(admin.ModelAdmin):
+    list_display=['id','name'] #리스트이거나 튜플이어야함
+    # list_display=('id',)
 
 admin.site.register(User)
 admin.site.register(UserProfile)
-admin.site.register(Hobby)
+admin.site.register(Hobby,HobbyAdmin) #Hobby라는 모델을 쓰고 HobbyAdmin 규칙 사용
 
 
 # Unregister
