31cfabad9a527016322dc924a742da57c1d86431
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-13 19:15:42 +0900

    Feat: APIView를 사용한 view&permissions 커스텀 연습

--- quest/views.py
+++ quest/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework import permissions
+from rest_framework.response import Response
 
-# Create your views here.
-def quest(request):
-    return JsonResponse({'message':'success'})
+#permission 커스텀
+class MyGoodPermission(permissions.BasePermission):
+    """
+    Allow any access.
+    This isn't strictly required, since you could use an empty
+    permission_classes list, but it's useful because it makes the intention
+    more explicit.
+    """
+    def has_permission(self, request, view):
+        user=request.user
+        result=bool(request.user and request.user.is_authenticated and user.permission_rank>10)
+        return
+
+
+#APIView를 사용하면 get, post, put, delete를 함수로 정의하여 사용할수있다.
+class UserApiView(APIView):
+    permission_classes=[MyGoodPermission]
+    # permission_classes = [permissions.AllowAny]
+    # permission_classes=[permissions.IsAdminUser]
+    # permission_classes=[permissions.IsAuthenticated]
+
+    def get(self,request):
+        return Response({'message':'get method!'})
+
+    def post(self,request):
+        return Response({'message':'post method!'})
+
+    def put(self,request):
+        return Response({'message':'put method!'})
+
+    def delete(self,request):
+        return Response({'message':'delete method!'})
