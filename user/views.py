280dccf566e1b8156be2f9b176efaa02c810e697
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-14 19:08:24 +0900

    정참조&역참조 공부

--- user/views.py
+++ user/views.py
@@ -5,10 +5,13 @@ from rest_framework.response import Response
 
 from rest_framework import permissions
 from rest_framework import status
+from .models import UserProfile as UserProfileModel
+from .models import Hobby as HobbyModel
+from .models import User as UserModel
+from django.db.models import F
+
 from django.contrib.auth import login
 from django.conf import settings
-
-from .models import Hobby
 
 #permission 커스텀
 class MyGoodPermission(permissions.BasePermission):
@@ -32,18 +35,34 @@ class UserApiView(APIView):
     # permission_classes=[permissions.IsAuthenticated] #로그인한경우
 
     def get(self,request):
-        print(settings.AUTH_USER_MODEL)
+        '''
+        #정참조
+        userprofile=UserProfileModel.objects.get(id=1)
+        print(userprofile.introduction)
+        print(userprofile.age)
+
+        #역참조
+        hobby=HobbyModel.objects.get(id=1) #운동
+        hobby_member=hobby.userprofile_set
+        '''
+
+        user = UserModel.objects.get(id=1)  #현재 AnonymousUser
+        hobbys=HobbyModel.objects.all()
+        # hobbys= user.userprofile.hobby.all()
+
+
+        for hobby in hobbys:
+            hobby_members=hobby.userprofile_set.exclude(user=user).annotate(username=F('user__username')).values_list('username',flat=True)
+            hobby_members=list(hobby_members)
+            # print(dir(user))
+            print(f"hobby:{hobby.name}/hobby members:{hobby_members}")
+
+
+
         try:
-            one_hobby,created=Hobby.objects.get_or_create(id="406")
+            one_hobby,created=HobbyModel.objects.get_or_create(id="406")
 
-            if created: #만들어야하면
-                one_hobby.name="달리기"
-                one_hobby.save()
-                print("생성완료")
-            else: #이미 존재하면
-                print(one_hobby)
-
-        except Hobby.DoesNotExist:
+        except HobbyModel.DoesNotExist:
             #object가 존재하지 않을때 이벤트
             return Response({'error': "존재하지 않는 hobby입니다."},status=status.HTTP_400_BAD_REQUEST)  #status클래스를 통해 가독성,생산성 좋아짐
             # return Response({'error': "존재하지 않는 hobby입니다."},status=400)
