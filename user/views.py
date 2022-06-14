d9ca129ed4abfe8fc4d1406fc17bcd147cd545d5
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-14 10:55:06 +0900

    custom user 생성

--- user/views.py
+++ user/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 
 from rest_framework import permissions
 from rest_framework import status
+from django.contrib.auth import login
+from django.conf import settings
 
 from .models import Hobby
 
@@ -30,6 +32,7 @@ class UserApiView(APIView):
     # permission_classes=[permissions.IsAuthenticated] #로그인한경우
 
     def get(self,request):
+        print(settings.AUTH_USER_MODEL)
         try:
             one_hobby,created=Hobby.objects.get_or_create(id="406")
 
