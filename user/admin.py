d1fcea6a2b4ccf5c5b5f7a7bdec83794a68f6210
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-22 17:15:11 +0900

    admin 심화 공부하기

--- user/admin.py
+++ user/admin.py
@@ -2,13 +2,32 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from .models import User,UserProfile,Hobby
 
+# class UserProfileInline(admin.TabularInline): #가로
+class UserProfileInline(admin.StackedInline): #세로
+    model=UserProfile
+    readonly_fields = ('birthday',)
+
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display=('id','username')
+    list_display_links = ('username',)
+    list_filter=('fullname',)
+    search_fields=('username',)
+    readonly_fields = ('username','password','join_date',)
+    fieldsets = (
+        ("info",{'fields':('username','email','password','fullname','join_date')}),
+        ('permissions',{'fields':('is_active','is_admin')})
+    )
+    inlines = (
+        UserProfileInline,
+    )
+
 class HobbyAdmin(admin.ModelAdmin):
     list_display=['id','name'] #리스트이거나 튜플이어야함
     # list_display=('id',)
 
-admin.site.register(User)
+admin.site.register(User,UserAdmin)
 admin.site.register(UserProfile)
 admin.site.register(Hobby,HobbyAdmin) #Hobby라는 모델을 쓰고 HobbyAdmin 규칙 사용
 
