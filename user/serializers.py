6d94df896033512375fde9d0c7d94066fd25a1f1
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-22 16:05:21 +0900

    serializer에 내가 지정한 필드 생성

--- user/serializers.py
+++ user/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 from .models import User,UserProfile,Hobby
+
 class HobbySerializer(serializers.ModelSerializer):
+    #내가 지정한 필드를 가져오고 싶다
+    my_custom_field=serializers.SerializerMethodField()
+
+    def get_my_custom_field(self,obj):
+        #각 유저의 유저프로필에 있는 취미 class 객체 출력
+        print(f"{obj}/{type(obj)}")
+        return "my_custom_field"
 
     class Meta:
         model=Hobby
-        fields=["name"]
+        fields=["name","my_custom_field"]
 
 class UserProfileSerializer(serializers.ModelSerializer):
     hobby=HobbySerializer(many=True)
